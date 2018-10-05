682979cb5cb571ed771e2f1f3bbe9cc1d3bd6b71
Konrad <konrad.trade@gmail.com>
2018-10-05 20:58:28 +0200

    Added working functionality

--- scripts/script.js
+++ scripts/script.js
@@ -13,5 +13,27 @@ function debounce(func, wait = 20, immediate = true) {
     };
   }
 
-  console.log('Error')
-  
+const sliderImages = document.querySelectorAll('.slide-in');
+
+function checkSlide(e) {
+
+  sliderImages.forEach(image => {
+
+    // Half way through the image
+    const slideInAt = (window.scrollY + window.innerHeight);
+    image.height / 2;
+
+    // Bottom of the image
+    const imageBottom = image.offsetTop + image.height;
+    const isHalfShown = slideInAt > image.offsetTop;
+    const isNotScrolledPast = window.scrollY < imageBottom;
+
+    if(isHalfShown && isNotScrolledPast){
+      image.classList.add('active');
+    } else {
+      image.classList.remove('active');
+    }
+  })
+}
+
+window.addEventListener('scroll', debounce(checkSlide));
